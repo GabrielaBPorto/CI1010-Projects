305dfd5b4c39918817617ef15d5f1897cc2bd62e
GABRIELA BAIRROS PORTO <gbp16@inf.ufpr.br>
2023-04-27 16:07:14 -0300

    feature: basic t2 structure

---
+++ Trabalho 2/database-creation.sh
@@ -0,0 +1,4 @@
+rails generate migration CreateValues taxes:float partial_value:float;
+rails generate migration CreateIngredients type:string;
+rails generate migration CreateRecipes name:string avg_time:integer type:string
+rails generate migration CreateRequests requester:string type:string table:string

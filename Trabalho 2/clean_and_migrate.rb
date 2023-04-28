b119d8fcdb21fd65f17a57e127752553357523c6
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-04-27 22:06:49 -0300

    litorina

--- Trabalho 2/clean_and_migrate.rb
+++ Trabalho 2/clean_and_migrate.rb
@@ -6,12 +6,12 @@ ActiveRecord::Base.establish_connection(
 )
 
 ActiveRecord::Base.connection.tables.each do |table|
-  puts ActiveRecord::Base.connection.inspect
+  # puts ActiveRecord::Base.connection.inspect
   ActiveRecord::Base.connection.execute("DROP TABLE #{table}")
 end
 
 require_relative 'migrations/migrate'
 
-Migrate.change
+# Migrate.change
 
 puts 'Done cleaning and running the migrations. You can see the database in db/development.sqlite3'

04f6c01d8e80e7088cb28d3cd829b83aa60548d1
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-04-27 22:08:39 -0300

    litorina

--- Trabalho 2/clean_and_migrate.rb
+++ Trabalho 2/clean_and_migrate.rb
@@ -1,22 +1,18 @@
 require 'active_record'
 require 'fileutils'
 
-dir_path = File.dirname(__FILE__)
-files = Dir.glob(File.join(dir_path, "*"))
-puts files
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: './db/development.sqlite3'
+)
 
-# ActiveRecord::Base.establish_connection(
-#   adapter: 'sqlite3',
-#   database: 'db/development.sqlite3'
-# )
+ActiveRecord::Base.connection.tables.each do |table|
+  # puts ActiveRecord::Base.connection.inspect
+  ActiveRecord::Base.connection.execute("DROP TABLE #{table}")
+end
 
-# ActiveRecord::Base.connection.tables.each do |table|
-#   # puts ActiveRecord::Base.connection.inspect
-#   ActiveRecord::Base.connection.execute("DROP TABLE #{table}")
-# end
+require_relative 'migrations/migrate'
 
-# require_relative 'migrations/migrate'
-
-# Migrate.change
+Migrate.change
 
 puts 'Done cleaning and running the migrations. You can see the database in db/development.sqlite3'

385bb03d7f27214b603d2c3cf1d08f0d914ef9cb
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-05-11 19:25:19 -0300

    feature: closer to finishing part 1

--- Trabalho 3/script.js
+++ Trabalho 3/script.js
@@ -1,28 +1,26 @@
 
 let canvas;
 let ctx;
-let line;
-let selectedPoint;
-let x1, y1;
-let computedStyle;
-let borderLeft;
-let borderTop;
-let paddingLeft;
-let paddingTop;
-let marginLeft;
-let marginTop;
-let firstline;
-const LINE_WIDTH = 5;
+let lines = [];
+const LINE_WIDTH = 1;
 const HALF_LINE_WIDTH = LINE_WIDTH / 2;
 
-let startX, startY, endX, endY;
-let draggingStart = false;
-let draggingEnd = false;
-let draggingLine = false;
-let mouseOffsetX, mouseOffsetY;
+let dragging = false;
+const fadeDuration = 250;
 
 
-function drawLine(x1, y1, x2, y2) {
+
+   
+// function isMouseOnStart(x, y) {
+// 	return Math.sqrt((x - startX) ** 2 + (y - startY) ** 2) < HALF_LINE_WIDTH;
+//   }
+  
+//   function isMouseOnEnd(x, y) {
+// 	return Math.sqrt((x - endX) ** 2 + (y - endY) ** 2) < HALF_LINE_WIDTH;
+//   }
+
+function drawLine({x1, y1, x2, y2}) {
+	console.log(`desenhando uma linha (${x1}, ${y1}) ->(${x2}, ${y2})`)
 		ctx.beginPath();
 		ctx.moveTo(x1, y1);
 		ctx.lineTo(x2, y2);
@@ -30,66 +28,75 @@ function drawLine(x1, y1, x2, y2) {
 		ctx.stroke();
   }
 
-  function getMousePos(canvas, evt) {
+
+  function getDistanceFromLine({x, y}, line) {
+	const { x1, y1, x2, y2 } = line;
+	const A = x - x1;
+	const B = y - y1;
+	const C = x2 - x1;
+	const D = y2 - y1;
+  
+	const dot = A * C + B * D;
+	const lenSq = C * C + D * D;
+	let param = -1;
+  
+	if (lenSq !== 0) {
+	  param = dot / lenSq;
+	}
+  
+	let xx, yy;
+  
+	if (param < 0) {
+	  xx = x1;
+	  yy = y1;
+	} else if (param > 1) {
+	  xx = x2;
+	  yy = y2;
+	} else {
+	  xx = x1 + param * C;
+	  yy = y1 + param * D;
+	}
+  
+	const dx = x - xx;
+	const dy = y - yy;
+	return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  function getMousePos(evt) {
 	const rect = canvas.getBoundingClientRect();
 	return {
 	  x: evt.clientX - rect.left,
 	  y: evt.clientY - rect.top
 	};
   }
-  
-  function isMouseOnStart(x, y) {
-	return Math.sqrt((x - startX) ** 2 + (y - startY) ** 2) < HALF_LINE_WIDTH;
+    
+  function isMouseOnLineStart({mouseX, mouseY}, line) {
+	const distance = getDistanceFromLine({mouseX, mouseY}, line);
+	const threshold = 100;
+	return distance <= threshold;
   }
   
-  function isMouseOnEnd(x, y) {
-	return Math.sqrt((x - endX) ** 2 + (y - endY) ** 2) < HALF_LINE_WIDTH;
-  }
+
   
-  function isMouseOnLine(x, y) {
-	const a = endY - startY;
-	const b = startX - endX;
-	const c = endX * startY - startX * endY;
-	const d = Math.abs(a * x + b * y + c) / Math.sqrt(a ** 2 + b ** 2);
-	return d < HALF_LINE_WIDTH;
-  }
-  
+
   function onMouseDown(evt) {
-	const mousePos = getMousePos(canvas, evt);
-	if (isMouseOnStart(mousePos.x, mousePos.y)) {
-	  draggingStart = true;
-	} else if (isMouseOnEnd(mousePos.x, mousePos.y)) {
-	  draggingEnd = true;
-	} else if (isMouseOnLine(mousePos.x, mousePos.y)) {
-	  draggingLine = true;
-	  mouseOffsetX = mousePos.x - startX;
-	  mouseOffsetY = mousePos.y - startY;
+	const mousePos = getMousePos(evt)
+	if(!dragging){
+		dragging = true
+	}
+	for(const line of lines){
+		console.log('line',line)
+		if(isMouseOnLineStart(mousePos, line)){
+			console.log('IS CLOSE')
+		}
 	}
   }
   
   function onMouseMove(evt) {
-	const mousePos = getMousePos(canvas, evt);
-	if (draggingStart) {
-	  startX = mousePos.x;
-	  startY = mousePos.y;
-	  drawLine(startX, startY, endX, endY);
-	} else if (draggingEnd) {
-	  endX = mousePos.x;
-	  endY = mousePos.y;
-	  drawLine(startX, startY, endX, endY);
-	} else if (draggingLine) {
-	  startX = mousePos.x - mouseOffsetX;
-	  startY = mousePos.y - mouseOffsetY;
-	  endX = startX + (endX - startX);
-	  endY = startY + (endY - startY);
-	  drawLine(startX, startY, endX, endY);
-	}
+	
   }
   
   function onMouseUp(evt) {
-	draggingStart = false;
-	draggingEnd = false;
-	draggingLine = false;
   }
 
 
@@ -97,32 +104,47 @@ function drawLine(x1, y1, x2, y2) {
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
 
-	const refreshButtonC = document.getElementById("refresh-button-clear");
-	refreshButtonC.addEventListener("click", refreshCanvas);
+	// const refreshButtonC = document.getElementById("refresh-button-clear");
+	// refreshButtonC.addEventListener("click", () => refreshCanvas(() =>));
 
 	const refreshButton = document.getElementById("refresh-button-one-line");
-	refreshButton.addEventListener("click", refreshCanvas);
-
+	refreshButton.addEventListener("click", () => refreshCanvas(addOneLine));
 
 	canvas.addEventListener("mouseup", onMouseUp);
-	canvas.addEventListener("mousemove", onMouseMove)
-	
+	canvas.addEventListener("mousemove", onMouseMove);
+	canvas.addEventListener("mousedown", onMouseDown);
 
-	refreshCanvasOneLine()
-
+	refreshCanvas(addOneLine)
   }
 
-  function refreshCanvasOneLine(){
-	refreshCanvas();
-	firstline = {
-		x1: canvas.width/4,
-		y1: canvas.height/4,
-		x2: canvas.width/2,
-		y2: canvas.height/2
+  function addOneLine(){
+	let line = {
+		x1: 50,
+		y1: 50,
+		x2: canvas.width-100,
+		y2: canvas.height-50
 	}
-	drawLine(firstline);
+	lines.push(line)
+	drawLine(line);
   }
   
-  function refreshCanvas() {
+  function refreshCanvas(lineFunction) {
+	fadeOutCanvas();
+	setTimeout(() => {
+		lineFunction();
+		fadeInCanvas();
+	}, fadeDuration);
+	lines = []
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
+
+  function fadeOutCanvas() {
+	canvas.classList.add("fade-out");
+	canvas.classList.remove("fade-in");
+  }
+  
+  function fadeInCanvas() {
+	canvas.classList.add("fade-in");
+	canvas.classList.remove("fade-out");
+  }
+  

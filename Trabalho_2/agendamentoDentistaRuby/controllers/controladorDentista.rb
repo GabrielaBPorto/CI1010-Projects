cb5be2a69de6782cafe8e513793a1ed69bd55b41
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-05-28 20:37:53 -0300

    fix ??

--- Trabalho_2/agendamentoDentistaRuby/controllers/controladorDentista.rb
+++ Trabalho_2/agendamentoDentistaRuby/controllers/controladorDentista.rb
@@ -1,10 +1,10 @@
 require_relative '../classes/dentista.rb'
 
 def imprimeInformacaoDentista(acao,dentista)
-    puts " Foi #{acao} dentista com nome #{dentista.nome} e cobra #{dentista.custo_por_procedimento} por procedimento feito e o telefone para contato é #{dentista.telefone}"
+    puts " Foi #{acao} dentista com nome #{dentista.nome} e cobra #{dentista.custo_por_procedimentos} por procedimento feito e o telefone para contato é #{dentista.telefone}"
 end
 def criarDentista(dados)
-    nome, custo_por_procedimento, telefone = dados.split(',')
+    nome, custo_por_procedimentos, telefone = dados.split(',')
     dentista = Dentista.find_by_nome(nome)
     if(dentista)
         puts "dentista já cadastrado"
@@ -14,19 +14,19 @@ def criarDentista(dados)
         telefone = '(41)99999-9999'
     end
     dentista = Dentista.new({:nome => nome,
-        :custo_por_procedimento => custo_por_procedimento.to_f,
+        :custo_por_procedimentos => custo_por_procedimentos.to_f,
         :telefone => telefone})
     dentista.save
     imprimeInformacaoDentista('criado',dentista)
 end
 def editarDentista(dados)
-    nome, custo_por_procedimento, telefone = dados.split(',')
+    nome, custo_por_procedimentos, telefone = dados.split(',')
     dentista = Dentista.find_by_nome(nome)
     if(!dentista)
         puts "O dentista com esse nome não existe"
         return
     end
-    dentista.custo_por_procedimento = custo_por_procedimento.to_f
+    dentista.custo_por_procedimentos = custo_por_procedimentos.to_f
     dentista.telefone = telefone
     dentista.save
     imprimeInformacaoDentista('editado',dentista)

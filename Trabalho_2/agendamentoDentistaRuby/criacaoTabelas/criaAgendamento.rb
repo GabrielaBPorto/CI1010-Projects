776bfa084744b4b7ce470b9f62efa8cb8842e99b
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-05-28 21:35:58 -0300

    fix ??

--- Trabalho_2/agendamentoDentistaRuby/criacaoTabelas/criaAgendamento.rb
+++ Trabalho_2/agendamentoDentistaRuby/criacaoTabelas/criaAgendamento.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection :adapter => "sqlite3",
-                                        :database => "../Tabelas.sqlite3"
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "../Tabelas.sqlite3"
+)
 
-ActiveRecord::Base.connection.create_table :agendamentos do |t|
+ActiveRecord::Base.connection.create_table :agendamentos, force: true do |t|
   t.datetime :data
-  t.references :paciente, foreign_key: { to_table: :pacientes }
-  t.references :dentista, foreign_key: { to_table: :dentistas }
-  t.references :procedimento, foreign_key: { to_table: :procedimentos }
+  t.references :paciente, foreign_key: true
+  t.references :dentista, foreign_key: true
+  t.references :procedimento, foreign_key: true
 end

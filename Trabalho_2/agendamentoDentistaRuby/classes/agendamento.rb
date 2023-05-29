9212f89154cf1c1541df3f8421c66d664ab65faa
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-05-28 21:39:20 -0300

    fix ??

--- Trabalho_2/agendamentoDentistaRuby/classes/agendamento.rb
+++ Trabalho_2/agendamentoDentistaRuby/classes/agendamento.rb
@@ -4,11 +4,9 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "Tabelas.sqlite3"
 )
-
 class Agendamento < ActiveRecord::Base
   self.table_name = 'agendamentos'
   belongs_to :paciente
+  belongs_to :dentista
   has_one :procedimento
-  has_many :dentistas
 end
-

f83fcfff83c248d7b6095e6a6bcdaa179be2f547
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-06-04 20:17:59 -0300

    feature: initial files for 4

---
+++ Trabalho 4/script.js
@@ -0,0 +1,34 @@
+const curriculumGrid = document.getElementById('curriculum-grid');
+const popup = document.getElementById('popup');
+const studentRAInput = document.getElementById('student-ra');
+
+let curriculumData = [];
+let studentData = [];
+
+function fetchCurriculumData(year) {
+}
+
+function fetchStudentData() {
+}
+
+function renderCurriculumGrid() {
+}
+
+function handleCellLeftClick(event) {
+}
+
+function handleCellRightClick(event) {
+}
+
+function handleStudentRAInputChange(event) {
+}
+
+
+curriculumGrid.addEventListener('click', handleCellLeftClick);
+curriculumGrid.addEventListener('contextmenu', handleCellRightClick);
+studentRAInput.addEventListener('input', handleStudentRAInputChange);
+
+
+function init() {
+  // fetchCurriculumData(1998); // Change the
+}
